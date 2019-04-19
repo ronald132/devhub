6cd65fee8e1a00db763dbeecf14d1656db7ec9b4
ronald132 <ronald132@gmail.com>
2019-04-19 18:39:03 +0800

    adding register and login api

---
+++ routes/api/users.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+router.get('/test', (req, res) => res.json({msg: 'it works'}));
+
+//@route  POST api/users/register
+//@desc   Register user
+//@access Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(user){
+        return res.status(400).json({email: "Email already exists"});
+      }else{
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', //Size
+          r: 'pg', //Rating
+          d: 'mm' //Default
+        });
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        });
+      }
+    });
+});
+
+//@route  POST api/users/login
+//@desc   User login / Returning JWT Token
+//@access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //find user by email
+  User.findOne({email})
+    .then(user => {
+      //Check for user
+      if(!user){
+        return res.status(404).json({email: 'User email not found'});
+      }
+
+      //Check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch){
+            res.json({msg: 'Success'});
+          }else{
+            return res.status(400).json({password: 'Password incorrect'});
+          }
+        })
+    })
+});
+
+
+module.exports = router;
