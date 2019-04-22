7bf465d2e9fd7b4e39ad72e9bffba49f41121994
ronald132 <ronald132@gmail.com>
2019-04-23 01:11:36 +0800

    Added login form

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,7 +8,7 @@ class Navbar extends Component {
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
           <div className="container">
             <Link className="navbar-brand" to="/">
-              DevConnector
+              DevHub
             </Link>
             <button
               className="navbar-toggler"
