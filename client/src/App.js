aa4b4002d9444217fb3caa48dc1d63552367e5bb
ronald132 <ronald132@gmail.com>
2019-04-27 00:23:36 +0800

    added redux

--- client/src/App.js
+++ client/src/App.js
@@ -7,22 +7,29 @@ import Login from './components/auth/Login';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
+ 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-        <Navbar/>
-        <Route exact path="/" component={ Landing } />
-        <div className="container">
-          <Route exact path="/register" component={ Register } />
-          <Route exact path="/login" component={ Login } />
-        </div>
-        <Footer/>
-        </div>
-      </Router>
+      <Provider store={ store }>
+        <Router>
+          <div className="App">
+          <Navbar/>
+          <Route exact path="/" component={ Landing } />
+          <div className="container">
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={ Login } />
+          </div>
+          <Footer/>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
