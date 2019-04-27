cab6ae6cade4e5afbd428da34c95dba96dd0e549
ronald132 <ronald132@gmail.com>
2019-04-27 23:20:03 +0800

    Testing Action and Dispatch

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 import axios from 'axios';
 import classnames from 'classnames';
+
+//connection redux to component
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
   constructor(){
@@ -31,18 +36,25 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     }
-    axios
-      .post('/api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({errors: err.response.data}));
+
+    this.props.registerUser(newUser);
+
+    // axios
+    //   .post('/api/users/register', newUser)
+    //   .then(res => console.log(res.data))
+    //   .catch(err => this.setState({errors: err.response.data}));
+
+
   }
 
   render() {
     const { errors } = this.state;
+    const { user } = this.props.auth;
 
     return (
       <div>
         <div className="register">
+          {user? user.name : null}
           <div className="container">
             <div className="row">
               <div className="col-md-8 m-auto">
@@ -121,4 +133,15 @@ class Register extends Component {
   }
 }
 
-export default Register;
+//to get state from our component, so we can access by this.props.auth
+
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) =>({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, {registerUser})(Register);
