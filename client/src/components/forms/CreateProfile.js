fbf8af2e598aa696759273d4bc169ca342ced2d7
ronald132 <ronald132@gmail.com>
2019-04-29 23:15:15 +0800

    Addde commons field: - SelectListGroup - TextAreaFieldGroup - InputGroup

--- client/src/components/forms/CreateProfile.js
+++ client/src/components/forms/CreateProfile.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
+import InputGroup from '../common/InputGroup';
+import SelectListGroup from '../common/SelectListGroup';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 
 class CreateProfile extends Component {
   constructor(props){
@@ -57,4 +60,4 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(null)(CreateProfile);
+export default connect(mapStateToProps)(CreateProfile);
