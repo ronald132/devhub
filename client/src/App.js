795b2f0b8487feb7063b0cfc3797a6ec6b82f380
ronald132 <ronald132@gmail.com>
2019-04-29 22:33:48 +0800

    Added Create Profile route

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/forms/CreateProfile';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/common/PrivateRoute';
@@ -53,6 +54,9 @@ class App extends Component {
             <Route exact path="/login" component={ Login } />
             <Switch>
               <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
             </Switch>
           </div>
           <Footer/>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -26,7 +26,7 @@ class Dashboard extends Component {
         dashboardContent = (
           <div>
             <p className="lead text-muted">Welcome {user.name}</p>
-            <p>You have not setup a profile, please add some info</p>
+            <p className="lead">You have not setup a profile, please add some info</p>
             <Link to="/create-profile" className="btn btn-lg btn-info">Create Profile</Link>
           </div>
         );
